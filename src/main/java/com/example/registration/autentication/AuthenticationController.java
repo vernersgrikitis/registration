190d978c5e5e1c352e7189c5366be371e42db630
Verners <vernersg@gmail.com>
2023-09-03 22:33:57 +0300

    added websocket functionality

--- src/main/java/com/example/registration/autentication/AuthenticationController.java
+++ src/main/java/com/example/registration/autentication/AuthenticationController.java
@@ -10,13 +10,11 @@ public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
-    @CrossOrigin
     @PostMapping("/registration")
     public void register(@RequestBody RegisterRequest request) {
         authenticationService.register(request);
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/logging-in")
     public AuthenticationResponse login(@RequestBody AuthenticationRequest request) {
         return authenticationService.authenticate(request);
